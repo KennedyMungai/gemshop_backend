593449666c7625f07b8b6e32335a4e8ae7409d6b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 11:47:31 +0300

    Wrote the basic code for the app

---
+++ app/main.py
@@ -0,0 +1,12 @@
+"""The entrypoint to the app""""
+from fastapi import FastAPI
+
+
+app = FastAPI(name='Gemshop Backend',
+              description="The backend of a gemshop application")
+
+
+@app.get("/", name="Root", description="The root endpoint for the application")
+async def root() -> dict[str, str]:
+  """The root endpoint for the application"""
+  return {"message": "Hello World"}
