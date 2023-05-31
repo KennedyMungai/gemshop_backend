57e59a53be09a6a832a76a895eedcc256a27b4b0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 12:35:27 +0300

    Created the file to hold the db connection data

--- database/db.py
+++ database/db.py
@@ -1,1 +1,7 @@
 """The db connection file"""
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False)
+
+Base = declarative_base()
