ffb08ae0c02db279c571cc48b1a4c9f21310bf18
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 12:42:55 +0300

    Called the function to create tables on app startup

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 """The entrypoint to the app"""
 from fastapi import FastAPI
 
+from database.db import create_db_and_tables
+
 app = FastAPI(name='Gemshop Backend',
               description="The backend of a gemshop application")
+
+
+@app.on_event("startup")
+def startup():
+    """Called on app startup"""
+    print("Starting up")
+    create_db_and_tables()
 
 
 @app.get("/", name="Root", description="The root endpoint for the application")
