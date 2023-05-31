db7445299d17d2c58104634017bbf4843bb7a7bf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 13:59:50 +0300

    Created the gem_props function

--- services/services.py
+++ services/services.py
@@ -1,5 +1,23 @@
 """The services file"""
-from models.gem_model import Gem
+from random import randint
+from string import ascii_uppercase
+
+from models.gem_model import Gem, GemProperties
+
+color_grades = ascii_uppercase[3:9]
+
+def create_gem_props():
+    """A function that fills in the gem properties
+
+    Returns:
+        gemp_-p: A gem instance
+    """
+    size = randint(3, 70) / 10
+    color = color_grades[randint(0, len(color_grades) - 1)]
+    clarity = randint(1, 4)
+    gemp_p = GemProperties(size=size, clarity=clarity, color=color)
+    
+    return gemp_p
 
 
 def create_gem(gem_p: int):
