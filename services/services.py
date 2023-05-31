757424615743f06bbfbe5a361e34cfc405162d56
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-31 13:10:59 +0300

    Added a docstring to the create_gem function

--- services/services.py
+++ services/services.py
@@ -3,6 +3,14 @@ from models.gem_model import Gem
 
 
 def create_gem(gem_p: int):
+    """Function to create gem instances
+
+    Args:
+        gem_p (int): An int passed
+
+    Returns:
+        Gem: A gem instance
+    """
     gem = Gem(price=1000, gem_properties_id=gem_p)
 
     return gem
